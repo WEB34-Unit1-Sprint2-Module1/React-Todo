6ff34a027d16c9cba2b0437e1013c153b186bb1a
alexis villaraza <alexis.borja@gmail.com>
2020-08-24 10:05:53 -0700

    finished to do form

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -3,12 +3,30 @@
 import React, {Component} from 'react';
 
 class TodoList extends Component {
+  constructor() {
+    super();
+    this.state = {
+      newTask: '',
+    };
+  }
+
+  handleChanges = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.addNewTask(this.state.newTask);
+    this.setState({ newTask: '' });
+  };
   
   render() {
     return (
-      <div>
-        <h2>To do list</h2>
-      </div>
+      <form onSubmit={this.handleSubmit}>
+        <input type='text' name='newTask' value={this.setState.newTask} onChange={this.handleChanges}/><button>Add Task</button>
+      </form>
     );
   }
 }
