90e81ab725ac884dd944111bc30d13cba1e1a87d
alexis villaraza <alexis.borja@gmail.com>
2020-08-24 10:11:42 -0700

    created tasks data and function for addNewTask

--- src/App.js
+++ src/App.js
@@ -1,17 +1,38 @@
 import React from 'react';
 import TodoForm from './components/TodoForm';
 import TodoList from './components/TodoList';
+import tasks from './tasks';
 
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+  constructor() {
+    super();
+    this.state = {
+      TodoList: tasks
+    }
+  }
+
+  addNewTask = taskText => {
+    const newTask = {
+      task: taskText,
+      id: Date.now(),
+      completed: false
+    }
+    this.setState({
+      TodoList: [...this.state.TodoList, newTask]
+    });
+  };
+
   render() {
+    console.log(this.state.TodoList)
+
     return (
       <div>
         <h2>Welcome to your Todo App!</h2>
-        <TodoForm />
+        <TodoForm addNewTask={this.addNewTask} />
         <TodoList />
       </div>
     );
---
+++ src/tasks.js
@@ -0,0 +1,14 @@
+const tasks = [
+  {
+    task: 'assignment 1',
+    id: 568432215,
+    completed: false
+  },
+  {
+    task: 'assignment 2',
+    id: 1687613549,
+    completed: false
+  }
+];
+
+export default tasks;
